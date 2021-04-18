c0bf04f736542c222a9ed3ca1cbc8ff9657e9e10
amir1226 <amir.sadour@jala-foundation.org>
2021-04-17 23:33:59 -0500

    Improving automapper with query and end section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -27,18 +27,14 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _userRepo.GetUsersAsync();
-
-            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-            return Ok(usersToReturn);
+            var users = await _userRepo.GetMembersAsync();
+            return Ok(users);
         }
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await _userRepo.GetUserByUsernameAsync(username);
-
-            return _mapper.Map<MemberDto>(user);
+            return await _userRepo.GetMemberAsync(username);
         }
     }
 }
