2b6a38fa9a66b9e01cbda1c123cdb9f4902a6b2e
Amir <amir1226@gmail.com>
2021-07-05 12:10:54 -0500

    Adding roles to JWT token first approach

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -43,10 +43,14 @@ namespace API.Controllers
 
             if(!result.Succeeded) return BadRequest(result.Errors);
 
+            var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+            if(!roleResult.Succeeded) return BadRequest(result.Errors);
+
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenservice.CreateToken(user),
+                Token = await _tokenservice.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -68,7 +72,7 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenservice.CreateToken(user),
+                Token = await _tokenservice.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
