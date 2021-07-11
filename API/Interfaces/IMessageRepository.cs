fd21aaa243a25b4a8e27c7b8cf09acf2689ba327
Amir <amir1226@gmail.com>
2021-07-10 23:01:42 -0500

    Fixing users notifications

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -12,6 +12,7 @@ namespace API.Interfaces
         void RemoveConnection(Connection connection);
         Task<Connection> GetConnection(string connectionId);
         Task<Group> GetMessageGroup(string groupName);
+        Task<Group> GetGroupForConnection(string connectionId);
         void AddMessage(Message message);
         void deleteMessage(Message message);
         Task<Message> GetMessage(int id);
--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using API.Extensions;
+using API.Helpers;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 
 namespace API.SignalR
@@ -18,17 +20,19 @@ namespace API.SignalR
         public override async Task OnConnectedAsync()
         {
             var isOnline = await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
-            if(isOnline) await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+            if (isOnline)
+                await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
 
             var currentUsers = await _tracker.GetOnlineUsers();
             await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
-
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
             var isOffline = await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
-            if(isOffline) await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+            
+            if (isOffline)
+                await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
 
             await base.OnDisconnectedAsync(exception);
         }
