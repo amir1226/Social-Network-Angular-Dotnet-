e98e3f17ab93bde92d3a1973195de43994e4edb6
Amir <amir1226@gmail.com>
2021-07-11 13:19:01 -0500

    Implementing unit of work to manage repositories

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -14,58 +14,48 @@ namespace API.Controllers
     [Authorize]
     public class MessagesController : BaseApiController
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IMessageRepository _messageRepository;
         private readonly IMapper _mapper;
-        public MessagesController(IUserRepository userRepository,
-                                IMessageRepository messageRepository, IMapper mapper)
+        private readonly IUnitOfWork _unitOfWork;
+        public MessagesController(IUnitOfWork unitOfWork, IMapper mapper)
         {
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
-            _messageRepository = messageRepository;
-            _userRepository = userRepository;
-
         }
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
-    {
-        messageParams.Username = User.GetUsername();
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUsername();
 
-        var messages = await _messageRepository.GetMessagesForUser(messageParams);
+            var messages = await _unitOfWork.MessageRepository.GetMessagesForUser(messageParams);
 
-        Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages);
+            Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages);
 
-        return messages;
+            return messages;
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMessage(int id)
+        {
+            var username = User.GetUsername();
+
+            var message = await _unitOfWork.MessageRepository.GetMessage(id);
+
+            if (message.Sender.UserName != username && message.Recipient.UserName != username)
+                return Unauthorized();
+
+            if (message.Sender.UserName == username) message.SenderDeleted = true;
+
+            if (message.Recipient.UserName == username) message.RecipientDeleted = true;
+
+            if (message.SenderDeleted && message.RecipientDeleted)
+                _unitOfWork.MessageRepository.deleteMessage(message);
+
+            if (await _unitOfWork.Complete()) return Ok();
+
+            return BadRequest("Failed to delete message");
+        }
+
     }
-
-    [HttpGet("thread/{username}")]
-    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
-    {
-        var currentUsername = User.GetUsername();
-        return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteMessage(int id)
-    {
-        var username = User.GetUsername();
-
-        var message = await _messageRepository.GetMessage(id);
-
-        if(message.Sender.UserName != username && message.Recipient.UserName != username)
-            return Unauthorized();
-
-        if(message.Sender.UserName == username) message.SenderDeleted = true;
-
-        if(message.Recipient.UserName == username) message.RecipientDeleted = true;
-
-        if(message.SenderDeleted && message.RecipientDeleted) 
-            _messageRepository.deleteMessage(message);
-            
-        if(await _messageRepository.SaveAllAsync()) return Ok();
-
-        return BadRequest("Failed to delete message");
-    }
-
-}
 }
